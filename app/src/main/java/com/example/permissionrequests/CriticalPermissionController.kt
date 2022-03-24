7ae89c17c82c65c12ce3599ac05f04e5e942dd4e
mehmetanilesen <mehmetanilesen@gmail.com>
2022-03-24 15:48:47 +0300

    Critical Permissions Request Caller

---
+++ app/src/main/java/com/example/permissionrequests/CriticalPermissionController.kt
@@ -0,0 +1,39 @@
+package com.example.permissionrequests
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+
+class CriticalPermissionController (private val activity: Activity,private val context : Context, private val requestCode : Int
+,private vararg var IntendedPermission : String)  {
+
+    private val RequestStatueHasMap : HashMap<String,Boolean>
+
+    init {
+        RequestStatueHasMap = hashMapOf()
+        PermissionStatueCheck()
+        RequestCaller()
+    }
+
+     fun PermissionStatueCheck(){
+        IntendedPermission.forEach {
+            RequestStatueHasMap[it] =
+                ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun RequestCaller(){
+        val Requestlist : ArrayList<String> = arrayListOf()
+
+        RequestStatueHasMap.forEach {
+            if(!it.value){
+                Requestlist.add(it.key)
+            }
+        }
+        println(Requestlist)
+        requestPermissions(activity,Requestlist.toTypedArray(),requestCode)
+    }
+
+}
---
+++ app/src/main/java/com/example/permissionrequests/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.permissionrequests
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CriticalPermissionController(this@MainActivity,this,0,Manifest.permission.BODY_SENSORS)
+
+    }
+}
